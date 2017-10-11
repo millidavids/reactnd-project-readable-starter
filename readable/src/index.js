d23ee91069c28d3c52fc06e6fb2ed088097d513e
millidavids <david.a.yurek@gmail.com>
2017-10-11 11:11:29 -0400

    feat: react routing and redux setup in the index of the application

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,34 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
-import { createStore, applyMiddleware, compose } from 'redux'
-import reducer from './reducers'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import reducers from './reducers'
+import App from './components/App'
+import './stylesheets/index.css'
 
-/**
-* @description Enhancers for redux
-*/
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-/**
-* @description The redux store initialization
-*/
 const store = createStore(
-  reducer,
+  reducers,
   composeEnhancers(
     applyMiddleware(thunk),
     applyMiddleware(logger),
   )
 )
 
-/**
-* @description Rendering the root components into the DOM
-*/
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route component={App}/>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
